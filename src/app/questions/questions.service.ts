d9c8a292f1fe57ae3d0240bd816a4d98f51c6d6e
ste2021 <estela.oliveira.cont@outlook.com>
2021-01-07 18:06:33 -0300

    obs

--- src/app/questions/questions.service.ts
+++ src/app/questions/questions.service.ts
@@ -1,6 +1,8 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Questions } from './questions';
 
 @Injectable({
   providedIn: 'root',
@@ -16,8 +18,8 @@ export class QuestionsService {
 //---------------- Http Methods---------------
   constructor(private http: HttpClient) { }
  
-  getUser(id) {
-    return this.http.get(`${this.rootUrl}/${id}`)
+  getUser(id): Observable<Questions[]> {
+    return this.http.get<Questions[]>(`${this.rootUrl}/${id}`)
   }
 
   
